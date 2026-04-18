e23ab3a88cfd4065079a76d586b49541f2ab7d0c
brunoverly <bsverly@gmai.com>
2026-04-18 16:17:00 -0300

    subindo o projeto para o github

---
+++ src/main/java/br/com/ClinicaFacilSaude/ClinicaFacilSaudeApplication.java
@@ -0,0 +1,13 @@
+package br.com.ClinicaFacilSaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaFacilSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaFacilSaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ClinicaFacilSaude/ClinicaFacilSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ClinicaFacilSaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaFacilSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
